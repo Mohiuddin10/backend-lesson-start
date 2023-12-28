776514c327664343820802ed2749b0120d56c912
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-12-28 20:50:25 +0600

    search by id added

--- users-management-server/index.js
+++ users-management-server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const services = require('./service.json')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,17 @@ app.get('/', (req, res) => {
 app.get('/user', (req, res) => {
     res.send('user is loading')
 });
+
+app.get('/services', (req, res) => {
+    res.send(services)
+})
+
+app.get('/services/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const service = services.find(service => service.id === id)
+    console.log(id);
+    res.send(service);
+})
 
 app.listen(port, ()=> {
     console.log(`server is running in port: ${port}`);
